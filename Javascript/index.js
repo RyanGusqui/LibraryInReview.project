e96af437a93704746e1573a7c6ac8919f1851224
evasquezASC <ernievazk7979@gmail.com>
2020-05-19 21:40:53 +0000

    Added html, css, and modified the Javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,13 @@
-let bookinfo = "https://www.googleapis.com/books/v1/";
-
-    fetch(bookinfo).then(function(response) {
+$("button").click(function() { 
+        let choice = $("#locatebook").val();
+        let bookinfo = "https://www.googleapis.com/books/v1/" + choice;
+    fetch(bookinfo)
+      .then(function(response) {
         return response.json();
-    }).then(function(data) {
-        console.log(data)
-    });
+
+      })
+      .then(function(items) {
+        $('body').append("<img src = " + items.imageLinks.thumbnail + ">")
+        $('body').append(items.description)    
+      })
+});
